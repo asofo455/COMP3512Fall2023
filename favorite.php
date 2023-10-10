bb810021955302664aed3e0e0c3894b008900741
hnijj156 <hnijj156@mtroyal.ca>
2023-10-10 12:36:33 -0600

    created favorite php page

---
+++ favorite.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html> 
+<head lang="en">
+    <meta charset="utf-8">
+    <meta name="authors" content="Athina Sofocleous & Harsheen Nijjer"/> 
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--title-->
+    <title>COMP 3512 Assignment 1</title>
+
+</head>
+
+<body>
+<header>
+    <nav class="flex-container">
+ 
+    </nav>
+
+
+</body>
