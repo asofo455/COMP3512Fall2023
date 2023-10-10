74e3e7646eba78b44e869f302a458027b852f04d
asofo455 <46725036+asofo455@users.noreply.github.com>
2023-10-10 13:17:46 -0600

    written in wrong section

--- index.php
+++ index.php
@@ -11,51 +11,8 @@
 <body>
 <header>
     <nav class="flex-container">
-        <!--links to other pages on the website--> 
-        <a href="index.php" target="_top"></i>Home</a>
-        <a href="search-page.php" target="_top"></i>Search</a> 
-        <a href="results-page.php" target="_top"></i>Browse </a>
-        <a href="favorites-page.php" target="_top"></i>Favorites</a>  
+ 
     </nav>
-</header>
 
-<main>
-<section>
-    <!--loop through SongDB class to retrieve info--> 
-    <?php foreach($songs as $s) {} ?>
-    <!--list for song information--> 
-    <h2 class="ui header">Here's the Song!</h2> 
-    <div class="container">
-    <ul class="description">
-        <li><span>Title: <?=$s['title'] ?></span><li>
-        <li><span>Artist Name: <?= $s['artist_name'] ?></span></li>
-        <li><span>Artist Type: <?= $s['type_name'] ?></span></li>
-        <li><span>Genre: <?= $s['genre_name'] ?></span></li>
-        <li><span>Year: <?= $s['year'] ?></span></li>
-        <li><span>Duration: <?= convertSeconds($s['duration']); ?></span></li>
 
-    </ul>
-
-    </div>
-
-    <!--list for song analysis and progress bars--> 
-    <h2 class="ui header">The Goods</h2> 
-    <div class="container">
-    <ul class="analysis">
-        <li><span>bpm <progress class="one" max="200" value="<?= $s['bpm']; ?>"></progress></span><span class="nums"><?= $s['bpm']; ?></span></li>
-        <li><span>energy <progress class="two" max="100" value="<?= $s['energy']; ?>"></progress></span><span class="nums"><?= $s['energy']; ?> / 100</span></li>
-        <li><span>danceability <progress class="three" max="100" value="<?= $s['danceability']; ?>"></progress></span><span class="nums"><?= $s['danceability']; ?> / 100</span></li>
-        <li><span>liveness <progress class="four" max="100" value="<?= $s['liveness']; ?>"></progress></span><span class="nums"><?= $s['liveness']; ?> / 100</span></li> 
-        <li><span>valence <progress class="five" max="100" value="<?= $s['valence']; ?>"></progress></span><span class="nums"><?= $s['valence']; ?> / 100</span></li> 
-        <li><span>acousticness <progress class="six" max="100" value="<?= $s['acousticness']; ?>"></progress></span><span class="nums"><?= $s['acousticness']; ?> / 100</span></li>
-        <li><span>speechiness <progress class="seven" max="100" value="<?= $s['speechiness']; ?>"></progress></span><span class="nums"><?= $s['speechiness']; ?> / 100</span></li> 
-        <li><span>popularity <progress class="eight" max="100" value="<?= $s['popularity']; ?>"></progress></span><span class="nums"><?= $s['popularity']; ?> / 100</span></li><br>
-</div>
-
-    </ul>
-    </div>
-
-</section>
-
-</main>
 </body>
