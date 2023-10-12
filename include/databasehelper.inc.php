9e98926b9f587fc08e8775536c28494d1398fcf3
hnijj156 <hnijj156@mtroyal.ca>
2023-10-11 18:55:48 -0600

    finish the database helper class

--- include/databasehelper.inc.php
+++ include/databasehelper.inc.php
@@ -80,11 +80,69 @@ class DatabaseHelper {
     }
 
     class SongsDB{
-        private static $baseSQL = "";
+        private static $baseSQL = "SELECT song_id, title, song.artist_id, artist_name, artisit_type_id,type_id, type_name, songs.genre_id, genre_name, year, bpm, energy, 
+        danceability, loudness, liveness, valence, duration, acousticness, speechiness, popularity
+        FROM (((songs
+        INNER JOIN artists ON songs.artist_id = artists.artist_id)
+        INNER JOIN genres ON songs.genre_id = genres.genre_id) INNER JOIN types ON artist_type_id = types.type_id) 
+     ";
 
         public function __construct($connection) {
             $this->pdo = $connection;
         }
+
+        // get all 
+        public function getAll(){
+            $sql = self::$baseSQL;
+            $statement = DatabaseHelper::runQuery($this->pdo, $sql, null);
+            return $statement->fetchAll();
+        }
+
+        // get song_id  
+        public function getSong($songID){
+            $sql = self::$baseSQL . "Where song_id=?";
+            $statement = DatabaseHelper::runQuery($this->pdo, $sql, Array($songID));
+            return $statement->fetchAll();
+        }
+
+        // get artist_id  
+        public function getAllForArtists($artistID){
+            $sql = self::$baseSQL . " Where songs.artist_id=?";
+            $statement = DatabaseHelper::runQuery($this->pdo, $sql, Array($artistID));
+            return $statement->fetchAll();
+        }
+
+        // get title 
+        public function getTitle($title){
+            $sql = self::$baseSQL . " WHERE title=?";
+            $sql .=  " OR title LIKE '%$title%'";
+            $sql .=  " ORDER BY artist_name ";
+            $statement = DatabaseHelper::runQuery($this->pdo, $sql, Array($title));
+            return $statement->fetchAll();
+        }
+
+        // get year 
+        public function getYear($year) {
+            $sql = self::$baseSQL . " WHERE year=?";
+            $statement = DatabaseHelper::runQuery($this->pdo, $sql, Array($year));
+            return $statement->fetchAll();
+        }
+
+        // get popularity 
+        public function getPopularity($popularity) {
+            $sql = self::$baseSQL . " WHERE popularity=?";
+            $statement = DatabaseHelper::runQuery($this->pdo, $sql, Array($popularity));
+            return $statement->fetchAll();
+        }
+
+        // get genre_id 
+        public function getAllForGenres($genreID) {
+            $sql = self::$baseSQL . " WHERE songs.genre_id=?";
+            $sql .=  " ORDER BY artist_name ";
+            $statement = DatabaseHelper::runQuery($this->pdo, $sql, Array($genreID));
+            return $statement->fetchAll();
+        }
+
     }
 
  ?>
