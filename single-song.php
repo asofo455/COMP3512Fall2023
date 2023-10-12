d0dd3c1dd7cdba1fc2d1c5cc7f92de97f010d711
hnijj156 <hnijj156@mtroyal.ca>
2023-10-11 22:26:06 -0600

    solved the foreach error

--- single-song.php
+++ single-song.php
@@ -48,7 +48,7 @@ catch (Exception $e) {
 <main>
 <section>
     <!--loop through SongDB class to retrieve info--> 
-    <?php foreach($songs as $s){}?>
+    <?php foreach($songs as $s){?>
     <!--list for song information--> 
     <h2 class="ui header">Here's the Song!</h2> 
     <div class="container">
@@ -78,7 +78,7 @@ catch (Exception $e) {
         <li><span>popularity <progress class="eight" max="100" value="<?= $s['popularity']; ?>"></progress></span><span class="nums"><?= $s['popularity']; ?> / 100</span></li><br>
 </div>
 
-    
+<?php } ?>
     
 
 </section>
