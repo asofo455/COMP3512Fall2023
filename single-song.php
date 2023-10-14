9d4cc8b6be947ce7460065391756920d0e87d8be
hnijj156 <hnijj156@mtroyal.ca>
2023-10-14 13:35:54 -0600

    fixing errro

--- single-song.php
+++ single-song.php
@@ -48,7 +48,8 @@ catch (Exception $e) {
 <main>
 <section>
     <!--loop through SongDB class to retrieve info--> 
-    <?php foreach($songs as $s){}?>
+
+    <?php foreach($songs as $s) { ?> 
     <!--list for song information--> 
     <h2 class="ui header">Here's the Song!</h2> 
     <div class="container">
@@ -77,6 +78,7 @@ catch (Exception $e) {
         <li><span>speechiness <progress class="seven" max="100" value="<?= $s['speechiness']; ?>"></progress></span><span class="nums"><?= $s['speechiness']; ?> / 100</span></li> 
         <li><span>popularity <progress class="eight" max="100" value="<?= $s['popularity']; ?>"></progress></span><span class="nums"><?= $s['popularity']; ?> / 100</span></li><br>
 </div>
+<?php } ?>
 </ul>
     </ul>
     </div>
