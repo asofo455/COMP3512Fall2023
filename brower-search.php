dd1d28ee17ed597154b51b7bf10d585931456fb0
hnijj156 <hnijj156@mtroyal.ca>
2023-10-14 14:15:39 -0600

    browser page

--- brower-search.php
+++ brower-search.php
@@ -11,7 +11,91 @@
 <body>
 <header>
     <nav class="flex-container">
- 
+  <!--links to other pages on the website-->
+  <a href="index.php" target="_top">Home</a>
+            <a class="current" href="search-page.php" target="_top">Search</a>
+            <a href="results-page.php" target="_top">Browse / Search Results</a>
+            <a href="favorites-page.php" target="_top">Favorites</a>
+        </nav>
+        </header>
+        <main>
+            <section>
+            <!--background image-->
+            <div class="image">
+                <div class="container">
+                    <!--form to conduct search and output to results-page.php-->
+                    <form action="results-page.php" method="get" target="_blank">
+                    <h2>Song Search</h2>
+                    <h4>Please select one option:</h4>
+                    <!--Search by title-->
+                    <div class="title">
+                    <label for="title">Title: </label>
+                    <input type="text" id="title" name='title' placeholder="Type in song title">
+                    </div>
+
+                    <!--Search by artist-->
+                    <div class="artist">
+                    <label for="artist">Artist: </label>
+                    <select id="artist" name="artist_id">
+                    <option value="" >Select artist</option>
+                    <?php
+                        foreach ($artists as $a) {
+                            echo "<option value='" . $a['artist_id'] . "'>" . $a['artist_name'] . "</option>";
+                         }
+                    ?>
+                    </select>
+                    </div>
+
+                    <!--Search by genre-->
+                    <div class="genre">
+                    <label for="genre">Genre: </label>
+                    <select id="genre" name='genre_id'>
+                    <option value="">Select genre</option>
+                    <?php
+                        foreach ($genres as $g) {
+                            echo "<option value='" . $g['genre_id'] . "'>" . $g['genre_name'] . "</option>";
+                        }
+                    ?>   
+                    </select>
+                    </div>
+
+                    <!--Search by year-->
+                    <div class="year">
+                    <label for="year">Year: </label>
+                    <!--Search year by less than-->
+                    <input type="radio" id="lessYear" name="choose1">
+                    <label for="lessYear">Less</label>
+                    <input type="number" id="lessYear" name='year' min="2017" max="2018">
+                    <!--Search year by greater than-->
+                    <input type="radio" id="greaterYear" name="choose1">
+                    <label for="greaterYear">Greater</label>
+                    <input type="number" id="greaterYear" name='year' min="2016" max="2018">
+                    </div>
+
+                    <!--Search by popularity score-->
+                    <div class="popularity">
+                    <label for="popularity">Popularity: </label>
+                    <!--Search by popularity score by less than-->
+                    <input type="radio" id="lessPopularity" name="choose1">
+                    <label for="lessPopularity">Less</label>
+                    <input type="number" id="lessPopularity" name='popularity' min="0" max="100">
+                    <!--Search by popularity score by greater than-->
+                    <input type="radio" id="greaterPopularity" name="choose1">
+                    <label for="greaterYear">Greater</label>
+                    <input type="number" id="greaterPopularity" name='popularity' min="0" max="100">
+                    </div>
+                
+                    <div class="buttons">
+                    <!--Search and redirect to results-page.php-->
+                    <input type="submit" value="Search" >
+                    <!--clear form-->
+                    <input type="reset">
+                    </div>
+                    </form>
+                </div>
+            </div>
+            </section>
+        </main>
     </nav>
 
 <!--github links, course name, and copyright--> 
