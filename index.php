f6734721542ae43d6ee774421f5ea4e122ec6175
hnijj156 <hnijj156@mtroyal.ca>
2023-10-19 09:53:44 -0600

    connecting css to php for index page

--- index.php
+++ index.php
@@ -9,6 +9,7 @@ require_once('include/databasehelper.inc.php');
     <meta charset="utf-8">
     <meta name="authors" content="Harsheen Nijjer"/> 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/index.css">
     <!--title-->
     <title>COMP 3512 Assignment 1</title>
 
