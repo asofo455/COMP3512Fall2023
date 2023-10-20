2dbae8115c0b14d3758ac626d0cacd4a8bcb2580
hnijj156 <hnijj156@mtroyal.ca>
2023-10-19 23:31:14 -0600

    favorite css

--- favorite.php
+++ favorite.php
@@ -34,6 +34,8 @@ try {
     <meta charset="utf-8">
     <meta name="authors" content=" Harsheen Nijjer"/> 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/favorite.css">
+
     <!--title-->
     <title>COMP 3512 Assignment 1</title>
 
